e3ef0e534f31cf5f24a3b5da60128cc8dfe67739
DanielMarcos1 <daniel04pessoal@gmail.com>
2024-01-08 15:10:58 -0300

    Basic entities

---
+++ src/main/java/entities/Role.java
@@ -0,0 +1,31 @@
+package entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document(collection = "Roles")
+public class Role implements Serializable {
+    @Id
+    private String id;
+    private String roleName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+     public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+
+}
---
+++ src/main/java/entities/User.java
@@ -0,0 +1,42 @@
+package entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document(collection = "Roles")
+public class User implements Serializable {
+    @Id
+    private String id;
+    private String userName;
+
+    @DBRef
+    private Role role;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
