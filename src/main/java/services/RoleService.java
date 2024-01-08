10c8908340f7aa28db6b33197a56b7d47d690462
DanielMarcos1 <daniel04pessoal@gmail.com>
2024-01-08 16:17:36 -0300

    Role and user services(Business logic)

---
+++ src/main/java/services/RoleService.java
@@ -0,0 +1,37 @@
+package services;
+
+import entities.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repositories.RoleRepository;
+import java.util.List;
+
+@Service
+public class RoleService {
+    @Autowired
+    RoleRepository roleRepository;
+
+    // Creates a new role
+    public Role createRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    // This method is self-explanatory, it finds the Role by its id or null if it doesn't exist.
+    public Role getRoleById(String id) {
+        return roleRepository.findById(id).orElse(null);
+    }
+
+    // Come on man do I really need to comment this?
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    // Updates an existing role in a database
+    public Role updateRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    public void deleteRole(String id) {
+        roleRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,39 @@
+package services;
+
+import entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    // Creates user
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // Finds user by id or null if it doesn't exist
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    // Method to find all users that returns it in a list
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Method to update a user
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+
+}
