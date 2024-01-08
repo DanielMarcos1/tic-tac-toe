40f20f115f7a9ce58d8da62e096236a8e2694f94
DanielMarcos1 <daniel04pessoal@gmail.com>
2024-01-08 12:04:29 -0300

    Initial commit

---
+++ src/test/java/com/tictactoe/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tictactoe.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
